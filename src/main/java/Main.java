647b7b0ff141b6eafa165da3aea4f51680d118e9
shyarnis <shyarnisgg88@gmail.com>
2024-07-22 08:00:49 +0545

    respond with 200

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,20 @@ import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
+        // initialize client socket.
+        Socket clientSocket = null;
+
         try {
             ServerSocket serverSocket = new ServerSocket(4221);
-            serverSocket.setReuseAddress(true);
 
-            serverSocket.accept(); // Wait for connection from client.
+            // client socket for accepting connection and responding 200 in bytes
+            clientSocket = serverSocket.accept();
+            clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+
             System.out.println("accepted new connection");
+
+            clientSocket.close();
+            serverSocket.close();
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
