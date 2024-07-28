58528bff2d35deff7e895de77d95e088e71f1ace
shyarnis <shyarnisgg88@gmail.com>
2024-07-28 21:22:03 +0545

    feat: read reqesut body i.e. POST /file/{filename}

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,17 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.lang.Thread;
-import java.lang.Runnable;
-import java.io.File;
 import java.nio.file.Files;
 
-/**
- * ClientRunnable
- */
 
 class ClientRunnable implements Runnable {
-    private Socket clientSocket;
-    private String directory;
+    private final Socket clientSocket;
+    private final String directory;
 
     public ClientRunnable(Socket socket, String directory) {
         this.clientSocket = socket;
@@ -23,6 +19,7 @@ class ClientRunnable implements Runnable {
     }
 
     // method overridding `run()`
+    @Override
     public void run() {
         // initialize input and output stream.
         BufferedReader inputStreamBufferedReader = null;
@@ -31,61 +28,118 @@ class ClientRunnable implements Runnable {
         // GET request variables.
         String requestLine = null;
         String pathName = null;
-        String subString = null;
         String userAgent = null;
         String headerLine = null;
+        String requestMethod = null;
+        int contentLength = 0;
 
         try {
             inputStreamBufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             textOutputWriter = new PrintWriter(clientSocket.getOutputStream(), true);
             requestLine = inputStreamBufferedReader.readLine();
             pathName = requestLine.split(" ")[1];
+            requestMethod = requestLine.split(" ")[0];
 
-            // find the user-agent
+            // find the user-agent abd content-length header
             while ((headerLine = inputStreamBufferedReader.readLine()) != null && !headerLine.isEmpty()) {
                 if (headerLine.startsWith("User-Agent: ")) {
                     userAgent = headerLine.substring("User-Agent: ".length());
+                } else if (headerLine.startsWith("Content-Length: ")) {
+                    contentLength = Integer.parseInt(headerLine.substring("Content-Length: ".length()));
                 }
             }
 
-            if (pathName.equals("/")) {
-
-                textOutputWriter.println("HTTP/1.1 200 OK\r\n\r\n");
-
-            } else if (pathName.startsWith("/echo")) {
-
-                subString = pathName.substring(6);
-                textOutputWriter.println("HTTP/1.1 200 OK\r\n" +
-                        "Content-Type: text/plain\r\n" +
-                        "Content-Length: " + subString.length() + "\r\n\r\n" + subString);
-
-            } else if (pathName.startsWith("/user-agent")) {
-
-                textOutputWriter.println("HTTP/1.1 200 OK\r\n" +
-                        "Content-Type: text/plain\r\n" +
-                        "Content-Length: " + userAgent.length() + "\r\n\r\n" + userAgent);
-
-            } else if (pathName.startsWith("/files")) {
-
-                // get fileName /files/{filename}
-                String fileName = pathName.substring(6);
-                File file = new File(directory, fileName);
-
-                if (file.exists()) {
-                    byte[] fileContent = Files.readAllBytes(file.toPath());
-                    textOutputWriter.println("HTTP/1.1 200 OK\r\n" +
-                            "Content-Type: application/octet-stream\r\n" +
-                            "Content-Length: " + fileContent.length + "\r\n\r\n" + new String(fileContent));
-                } else {
+            // read the body
+            StringBuilder bodyBuilder = new StringBuilder();
+            if (contentLength > 0) {
+                char[] bodyChars = new char[contentLength];
+                inputStreamBufferedReader.read(bodyChars, 0, contentLength);
+                bodyBuilder.append(bodyChars);
+            }
+            String body = bodyBuilder.toString();
+            
+            // handle request method
+            switch (requestMethod) {
+                case "GET" ->
+                    handleGetRequest(textOutputWriter, pathName, userAgent, directory);
+                case "POST" -> 
+                    handlePostRequest(textOutputWriter, pathName, body, directory);
+                default -> 
                     textOutputWriter.println("HTTP/1.1 404 Not Found\r\n\r\n");
+            }
+
+            inputStreamBufferedReader.close();
+            textOutputWriter.close();
+
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
+
+    // handle get request
+    private void handleGetRequest(PrintWriter textOutputWriter, String pathName, String userAgent, String directory) {
+
+        if (pathName.equals("/")) {
+
+            textOutputWriter.println("HTTP/1.1 200 OK\r\n\r\n");
+
+        } else if (pathName.startsWith("/echo")) {
+            // GET /echo/{substring}
+            String subString = pathName.substring(6);
+            textOutputWriter.println("""
+                                     HTTP/1.1 200 OK\r
+                                     Content-Type: text/plain\r
+                                     Content-Length: """ + subString.length() + "\r\n\r\n" + subString);
+
+        } else if (pathName.startsWith("/user-agent")) {
+
+            textOutputWriter.println("""
+                                     HTTP/1.1 200 OK\r
+                                     Content-Type: text/plain\r
+                                     Content-Length: """ + userAgent.length() + "\r\n\r\n" + userAgent);
+
+        } else if (pathName.startsWith("/files")) {
+            // GET /files/{filename}
+            String fileName = pathName.substring(6);
+            File file = new File(directory, fileName);
+
+            if (file.exists()) {
+                try {
+
+                    byte[] fileContent = Files.readAllBytes(file.toPath());
+                    textOutputWriter.println("""
+                                             HTTP/1.1 200 OK\r
+                                             Content-Type: application/octet-stream\r
+                                             Content-Length: """ + fileContent.length + "\r\n\r\n" + new String(fileContent));
+
+                } catch (IOException e) {
+                    textOutputWriter.println("HTTP/1.1 500 Internal Server Error\r\n\r\n");
                 }
 
             } else {
                 textOutputWriter.println("HTTP/1.1 404 Not Found\r\n\r\n");
             }
 
-            inputStreamBufferedReader.close();
-            textOutputWriter.close();
+        } else {
+            textOutputWriter.println("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
+    }
+
+    // handle POST Request
+    private void handlePostRequest(PrintWriter textOutputWriter, String pathName, String body, String directory) {
+        // POST /files/{filename}
+        String fileName = pathName.substring(7);
+        File file = new File(directory, fileName);
+
+        try {
+            if (file.createNewFile() || file.exists()) {
+                try (FileWriter fileWriter = new FileWriter(file, false)) {
+                    fileWriter.write(body);
+                }
+                textOutputWriter.println("HTTP/1.1 201 Created\r\n\r\n");
+            } else {
+                textOutputWriter.println("HTTP/1.1 404 Not Found\r\n\r\n");
+            }
 
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
